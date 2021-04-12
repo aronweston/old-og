307ae21b8f130fee3b53dae55f8b045ccc8a1b44
aronweston <aron@firststepdigital.com.au>
2021-04-12 21:16:53 +1000

    product update function

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,7 +1,8 @@
 const faunaDb = require('faunadb');
 const verifyWebhookIntegrity = require('shopify-verify-webhook');
+const axios = require('axios');
 
-const q = faunaDb.query;
+const query = faunaDb.query;
 const client = new faunaDb.Client({
   secret: process.env.FAUNADB_SECRET,
 });
@@ -14,10 +15,51 @@ exports.handler = function (event, context, callback) {
   );
 
   if (isValid) {
-    callback(null, {
-      statusCode: 200,
-      body: 'Hello world',
+    const body = JSON.parse(event.body);
+
+    const { id } = body;
+
+    delete body.updated_at;
+    body.variants.forEach(variant => {
+      delete variant.updated_at;
+      delete variant.inventory_quantity;
+      delete variant.old_inventory_quantity;
     });
+
+    const bodyString = JSON.stringify(body);
+
+    client
+      .query(query.Get(query.Match(q.Index('product_by_id'), id)))
+      .then(result => {
+        if (result.data.product !== bodyString) {
+          client
+            .query(
+              q.Update(result.ref, {
+                data: { product: bodyString },
+              })
+            )
+            .then(() => {
+              axios.post(process.env.NETLIFY_REBUILD_URL);
+            })
+            .catch(e => {
+              console.log('error updating product', e);
+            });
+        }
+      })
+      .catch(() => {
+        client
+          .query(
+            query.Create(query.Collection('products'), {
+              data: { id, product: bodyString },
+            })
+          )
+          .then(() => {
+            axios.post(process.env.NETLIFY_REBUILD_URL);
+          })
+          .catch(e => {
+            console.log('error adding to db:', e);
+          });
+      });
   } else {
     callback(null, {
       statusCode: 403,
